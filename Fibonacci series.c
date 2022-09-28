71022ec090b8583cea04bd79b53eb63d7b8a941a
Harjaspal Singh <71326879+Harjas-Sidhu@users.noreply.github.com>
2022-09-28 21:50:02 +0530

    Update Fibonacci series.c
    I think iterative approach is better than recursive for this case.

--- Fibonacci series.c
+++ Fibonacci series.c
@@ -1,20 +1,18 @@
-#include<stdio.h>    
-void printFibonacci(int n){    
-    static unsigned long n1=0,n2=1,n3;    
-    if(n>0){    
-         n3 = n1 + n2;    
-         n1 = n2;    
-         n2 = n3;    
-         printf("%lu ",n3);    
-         printFibonacci(n-1);    
-    }    
-}    
-int main(){    
-    int n;    
-    printf("Enter the number of elements: ");    
-    scanf("%d",&n);    
-    printf("Fibonacci Series: ");    
-    printf("%d %d ",0,1);    
-    printFibonacci(n-2);//n-2 because 2 numbers are already printed    
-  return 0;  
- }   
+#include <stdio.h>
+
+int a = 0, b = 1;
+int nexterm, num;
+
+int main()
+{
+    printf("Enter the number of terms you want to print\n");
+    scanf("%d", &num);
+    for (int c = 0; c < num; c++)
+    {
+        nexterm = a + b;
+        printf("%d\n", a+b);
+        a = b;
+        b = nexterm;
+    }
+    return 0;
+}
